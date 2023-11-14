725bea85ff4b71b827f38d5b810aee7ea64a9e81
BENNI-GITHUB <>
2023-11-14 20:11:40 +0000

    create while true to get the valid data without run the program

--- run.py
+++ run.py
@@ -12,31 +12,42 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPEAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPEAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figure input from the users.
     """
-    print("Please enter the sales data from the last market")
-    print("Data should be sixnumbers saparated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter the data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
 
-def validate_data(value):
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
+
+def validate_data(values):
     """
     Check the value amount is 6 and the value are int
     """
     try:
-        if len(value) != 6:
+        [int(value) for value in values]
+        if len(values) != 6:
             raise ValueError(
-        f"Exactly 6 values are required, you provided {len(value)}"
-        )
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
-        print(f"Invalid data: {e}, Please try again \n")
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-        
-get_sales_data()
+    return True
 
+
+data = get_sales_data()
