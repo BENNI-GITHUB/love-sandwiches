6c4ec58ed8549a8a257cb8ff8798b2cbc0070f07
BENNI-GITHUB <>
2023-11-16 11:14:20 +0000

    create function to update the surplus worksheet

--- run.py
+++ run.py
@@ -81,6 +81,16 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def update_surplus_worksheet(data):
+    """
+    Update the Surplus woorksheet and add a new row with the valid surplus data
+    """
+    print("Updating Surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully")
+
+
 def main():
     """
     Run all program functions.
@@ -89,7 +99,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Sandwich automation")
