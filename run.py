ed4635b73ba2a4d888a8275a46392d55b4fdda9e
BENNI-GITHUB <>
2023-11-16 11:32:39 +0000

    create a refactoring function for updating sales and surplus worksheet

--- run.py
+++ run.py
@@ -51,14 +51,35 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
+# def update_sales_worksheet(data):
+#     """
+#     Update the sales woorksheet and add a new row with the valid sales data
+#     """
+#     print("Updating Sales worksheet...\n")
+#     sales_worksheet = SHEET.worksheet("sales")
+#     sales_worksheet.append_row(data)
+#     print("Sales worksheet updated successfully")
+
+
+# def update_surplus_worksheet(data):
+#     """
+#     Update the Surplus woorksheet and add a new row with the valid surplus
+#     """
+#     print("Updating Surplus worksheet...\n")
+#     surplus_worksheet = SHEET.worksheet("surplus")
+#     surplus_worksheet.append_row(data)
+#     print("Surplus worksheet updated successfully")
+
+
+def update_worksheet(data, worksheet):
     """
-    Update the sales woorksheet and add a new row with the valid sales data
+    Update the woorksheets and add a new row with the valid data for
+    the requested worksheet
     """
-    print("Updating Sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully")
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully")
 
 
 def calculate_surplus_data(sales_row):
@@ -81,25 +102,15 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def update_surplus_worksheet(data):
-    """
-    Update the Surplus woorksheet and add a new row with the valid surplus data
-    """
-    print("Updating Surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully")
-
-
 def main():
     """
     Run all program functions.
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwich automation")
